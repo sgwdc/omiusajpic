ae1d06063ea30ee5c24a3fbeff5867e2465b59d1
Steven Greenwaters <swaters@livingstreets.com>
2016-11-01 12:25:24 -0400

    Upgrade Google Analytics code for old account

--- footer.php
+++ footer.php
@@ -20,23 +20,22 @@
     <?php wp_footer(); ?>
     </div><!-- end #footer -->
 </div>
-<!-- Old Analytics account that Christina Herman's email cherman@omiusa.org only has user level access to -->
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+<!-- Old Analytics account which Christina Herman's Google account cherman@omiusa.org has only user level access to -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-4947421-1', 'auto');
+  ga('send', 'pageview');
 </script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-4947421-1");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
+
 <!-- New Analytics account created 9/8/2016 -->
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
   ga('create', 'UA-83880741-1', 'auto');
   ga('send', 'pageview');
 </script>
