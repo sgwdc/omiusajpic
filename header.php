62cf38d02daba739eeabe7e372c30fa2b74c7773
Steven Greenwaters <swaters@livingstreets.com>
2016-11-02 12:01:29 -0400

    Replace browser icon of JPIC's old logo with OMI's logo (one for Internet Explorer, and one for everything else)

--- header.php
+++ header.php
@@ -18,7 +18,8 @@ if ($_REQUEST['member_logout']==1) member_logout();
 <link rel="feed alternate" type="application/rss+xml"
       href="<?php bloginfo_rss('rss_url') ?>" title="<?php bloginfo_rss('title') ?>">
 <meta name="description" content="The Missionary Oblates of Mary Immaculate's Justice, Peace, and Integrity of Creation effort is a global initiative to promote justice, equality, peace, and a clean and sustainable environment for all." />
-<link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/favicon.ico">
+<!--[if IE]><link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/images/favicon.ico"><![endif]-->
+<link rel="icon" href="<?php bloginfo('template_directory') ?>/images/favicon.png">
 <?php wp_head(); ?>
 </head>
 
