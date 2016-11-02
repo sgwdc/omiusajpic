6dd7bd49e2ce0f29845affd8c00c3940bb5d405c
Steven Greenwaters <swaters@livingstreets.com>
2016-11-02 17:12:49 -0400

    On homepage, strip shortcodes and whitespace from excerpts. Add a "Read more" link after each. Link each thumbnail. Italicize the date for each.

--- homepage.php
+++ homepage.php
@@ -45,15 +45,20 @@ get_header();
 					// If a "Featured Image" exists for this post, display it
 					if (has_post_thumbnail($post)) {
 						$thumbnailURL = get_the_post_thumbnail_url($post, $size="thumbnail");
-						echo '<img src="' . $thumbnailURL . '" style="float:left; padding:2px 5px 0 0;">';
+						echo '<a href="' . get_the_permalink() . '"><img src="' . $thumbnailURL . '" style="float:left; padding:2px 5px 0 0;"></a>';
 					}
 					// Display the post date
-					the_time('F jS, Y');
-					echo ' -- ';
+					echo '<em>' . get_the_time('F jS, Y') . ' -- </em>';
 					// Display an excerpt of the post content
-					$thecontent = wp_strip_all_tags(get_the_content(), false);
-					$excerpt = substr($thecontent, 0, 255);
-					echo $excerpt;
+					$thecontent = get_the_content();
+					// Strip HTML tags, and remove left over line breaks and white space
+					$thecontent = wp_strip_all_tags($thecontent, false);
+					// Strip shortcodes
+					$shortcode_pattern= '/\[(\/?caption.*?(?=\]))\]/';
+					$thecontent = preg_replace($shortcode_pattern, '', $thecontent);
+					// Take only the first 255 characters, and trim whitespace
+					$thecontent = ltrim(rtrim(substr($thecontent, 0, 255))) . "...";
+					echo $thecontent . '<br><a href="' . get_the_permalink() . '">Read more &gt;</a>';
 				?>
 				</p>
 				<div class="clearboth"></div>
