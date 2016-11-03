77f79ee9a4dd08cf60a5b34c2199ffcb29b2b5d0
Steven Greenwaters <swaters@livingstreets.com>
2016-11-02 22:23:52 -0400

    Remove code from functions.php that seems to be obsolete

--- functions.php
+++ functions.php
@@ -1,20 +1,5 @@
 <?php
 
-function nbm_global_page_id()
-{
-  static $runBefore, $thisID;
-  if (!$runBefore)
-  {
-    $runBefore = true;
-    global $post;
-    $thisID = $post->ID;
-  }
-  else
-    return $thisID;
-}
-
-add_action('wp_head','nbm_global_page_id');
-
 function omi_spanish_date($english_date) {
 	$months = array(
 		'January'=>'Enero',
@@ -38,18 +23,6 @@ function omi_spanish_date($english_date) {
 	return $spanish_date; 
 }
 
-function remove_private_prefix($title) {
-	$title = str_replace(
-	'Protected:',
-	'Members only:',
-	$title);
-	return $title;
-}
-
-add_filter('the_title','remove_private_prefix');
-remove_action('wp_head', 'wp_generator');
-
 // Enable "Featured Images" for pages and posts
 add_theme_support('post-thumbnails');
-
 ?>
