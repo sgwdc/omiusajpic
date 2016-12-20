10ecab09291f4224ae815014b82ea21139502d1d
Steven Greenwaters <swaters@livingstreets.com>
2016-12-20 09:10:23 -0500

    Simplify, especially by removing div.mainhome

--- homepage.php
+++ homepage.php
@@ -19,55 +19,51 @@ $hero_url = get_template_directory_uri() . '/images/' . $random_hero;
 	include 'navbar.php';
 ?>
 
-<!-- MAIN SECTION -->
-<div class="mainhome">
+<!-- Breadcrumb -->
+<div id="topbreadcrumb">
+	Homepage
+</div>
 
-	<!-- Breadcrumb -->
-	<div id="topbreadcrumb">
-		Homepage
-	</div>
-
-	<!-- MAIN CONTENT -->
-	<div class="homecontents">
-		<a name="maincontent"></a>
-		<div class="column" id="homepageleft">
-			<!-- News -->
-			<h3><a href="news" class="darkgraylink">Recent News &amp; Updates</a></h3>
+<!-- MAIN CONTENT -->
+<div class="homecontents">
+	<a name="maincontent"></a>
+	<div id="homepageleft">
+		<!-- News -->
+		<h3><a href="news" class="darkgraylink">Recent News &amp; Updates</a></h3>
+		<?php
+		// Get homepage posts 
+		$lastposts = get_posts('numberposts=3&category=90');
+		foreach($lastposts as $post) :
+			setup_postdata($post);
+			?>
+			<p class="clearfix">
+			<a href="<?php the_permalink(); ?>" class="big"><?php the_title(); ?></a><br>
 			<?php
-			// Get homepage posts 
-			$lastposts = get_posts('numberposts=3&category=90');
-			foreach($lastposts as $post) :
-				setup_postdata($post);
-				?>
-				<p class="clearfix">
-				<a href="<?php the_permalink(); ?>" class="big"><?php the_title(); ?></a><br>
-				<?php
-					// If a "Featured Image" exists for this post, display it
-					if (has_post_thumbnail($post)) {
-						$thumbnailURL = get_the_post_thumbnail_url($post, $size="thumbnail");
-						echo '<a href="' . get_the_permalink() . '"><img src="' . $thumbnailURL . '" style="float:left; padding:2px 5px 0 0;"></a>';
-					}
-					// Display the post date
-					echo '<em>' . get_the_time('F jS, Y') . ' -- </em>';
-					// Display an excerpt of the post content
-					$thecontent = get_the_content();
-					// Strip HTML tags, and remove left over line breaks and white space
-					$thecontent = wp_strip_all_tags($thecontent, false);
-					// Strip shortcodes
-					$shortcode_pattern= '/\[(\/?caption.*?(?=\]))\]/';
-					$thecontent = preg_replace($shortcode_pattern, '', $thecontent);
-					// Remove multiple spaces
-					$thecontent = preg_replace('/\s+/', ' ', $thecontent);
-					// Take only the first 255 characters, and trim whitespace
-					$thecontent = ltrim(rtrim(substr($thecontent, 0, 255))) . "...";
-					echo $thecontent . '<br><a href="' . get_the_permalink() . '">Read more &gt;</a>';
-				?>
-				</p>
-				<br>
-			<?php endforeach; ?>
+				// If a "Featured Image" exists for this post, display it
+				if (has_post_thumbnail($post)) {
+					$thumbnailURL = get_the_post_thumbnail_url($post, $size="thumbnail");
+					echo '<a href="' . get_the_permalink() . '"><img src="' . $thumbnailURL . '" style="float:left; padding:2px 5px 0 0;"></a>';
+				}
+				// Display the post date
+				echo '<em>' . get_the_time('F jS, Y') . ' -- </em>';
+				// Display an excerpt of the post content
+				$thecontent = get_the_content();
+				// Strip HTML tags, and remove left over line breaks and white space
+				$thecontent = wp_strip_all_tags($thecontent, false);
+				// Strip shortcodes
+				$shortcode_pattern= '/\[(\/?caption.*?(?=\]))\]/';
+				$thecontent = preg_replace($shortcode_pattern, '', $thecontent);
+				// Remove multiple spaces
+				$thecontent = preg_replace('/\s+/', ' ', $thecontent);
+				// Take only the first 255 characters, and trim whitespace
+				$thecontent = ltrim(rtrim(substr($thecontent, 0, 255))) . "...";
+				echo $thecontent . '<br><a href="' . get_the_permalink() . '">Read more &gt;</a>';
+			?>
+			</p>
+			<br>
+		<?php endforeach; ?>
 
-			<p><a href="/news/">See more news &amp; updates &gt;</a></p>
-		</div>
+		<p><a href="/news/">See more news &amp; updates &gt;</a></p>
 	</div>
 </div>
 
@@ -80,7 +76,7 @@ $hero_url = get_template_directory_uri() . '/images/' . $random_hero;
 	</div>
 	<a class="twitter-timeline" href="https://twitter.com/omiusaJPIC" data-widget-id="682618667486556160">Tweets by @omiusaJPIC</a>
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-</div><!-- end #mainhome -->
+</div>
 
 <br>
 <br>
