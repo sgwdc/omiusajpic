45ef3b78425fcc783bc1835081353d94d7adb3a7
Steven Greenwaters <swaters@livingstreets.com>
2017-01-12 00:44:37 -0500

    Spacing & code cleanup only.

--- sidebar-interior.php
+++ sidebar-interior.php
@@ -1,23 +1,13 @@
 <?php 
-// NO, do not hide this sidebar from the search results page
-//if (!is_page('search-results')) : 
-?>
+	// Set a variable so we know if this is the news page
+	global $newspage; 
+	$newspage = false; 
+	$page_uri = getenv('REQUEST_URI'); 
+	if (strstr($page_uri,'/news')) $newspage = true;
+	$news_cat = 90; 
+	$posts_num = 4;
+	$news_title = 'Recent News';
 
-<?php 
-// Set a variable so we know if this is the news page
-global $newspage; 
-$newspage = false; 
-$page_uri = getenv('REQUEST_URI'); 
-if (strstr($page_uri,'/news')) $newspage = true;
-?>
-
-<?php 
-$news_cat = 90; 
-$posts_num = 4;
-$news_title = 'Recent News';
-?>
-
-<?php 
 	// spanish
 	if (!$newspage && (is_page_template('spanish.php')||is_category(206)||in_category(206))) {
 		$news_cat = 206; 
@@ -44,101 +34,89 @@ $news_title = 'Recent News';
 <?php if(is_single() || !is_page()) $posts_num = 3; ?>
 
 <!-- News sidebar -->
-    <div id="interior-sidebar">
+<div id="interior-sidebar">
+	<?php
+		// Don't try to access the post ID if this is the 404 page, etc.
+		if (isset($post)) {
+			// Get any subpages for this page
+			$args = array(
+				'post_status' => 'publish',
+				'child_of' => $post -> ID,
+				'depth' => 1,
+				'title_li' => null,
+				'echo' => false
+			);
+			$subnav_menu = wp_list_pages( $args );
 
-	    <?php
-	    	// Don't try to access the post ID if this is the 404 page, etc.
-	    	if (isset($post)) {
-		    	// Get any subpages for this page
-		    	$args = array(
-		    		'post_status' => 'publish',
-		    		'child_of' => $post -> ID,
-		    		'depth' => 1,
-		    		'title_li' => null,
-		    		'echo' => false
-		    	);
-		    	$subnav_menu = wp_list_pages( $args );
-		    	
-		    	// If this page has any subpages, display them
-		    	if (strlen($subnav_menu)) {
-			    	echo '<h4>Sections</h4><ul>';
-			    	echo $subnav_menu;
-					echo '</ul>';
-		    	}
+			// If this page has any subpages, display them
+			if (strlen($subnav_menu)) {
+				echo '<h4>Sections</h4><ul>';
+				echo $subnav_menu;
+				echo '</ul>';
+			}
+		}
+	?>
 
-	    	}
-	    ?>
-
-
-        <h4><?php echo $news_title; ?></h4>
-        <ul>
-        <?php
-		// Get homepage posts 
-		 if ($news_cat != '206') $lastposts = get_posts('numberposts='.$posts_num.'&category='.$news_cat.',-206');
-		 else $lastposts = get_posts('numberposts='.$posts_num.'&category='.$news_cat);
-		 foreach($lastposts as $post) :
+	<h4><?php echo $news_title; ?></h4>
+	<ul>
+		<?php
+			// Get homepage posts 
+			if ($news_cat != '206') $lastposts = get_posts('numberposts='.$posts_num.'&category='.$news_cat.',-206');
+			else $lastposts = get_posts('numberposts='.$posts_num.'&category='.$news_cat);
+			foreach($lastposts as $post) :
 			setup_postdata($post);
-		 ?>
-         <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
-		 <?php if ($news_cat != '206') : ?><?php the_time('F jS, Y') ?>
-		 <?php else: ?><?php echo omi_spanish_date(the_date('j F Y','','',FALSE)); ?>
-		 <?php endif; ?></li>
-         <?php endforeach; ?>
-
-        </ul>
-        
-       
-        
-        
-		<?php // Show more links only on the News pages ?>
-		<?php if(is_single() || !is_page()) : ?>
-        <h4>News Feed</h4>
-        <ul><li><a href="<?php bloginfo_rss('rss_url') ?>">Subscribe to RSS/XML feed</a></li></ul>
-        <h4>News Archives</h4>
-        <!--<ul><?php wp_get_archives('type=monthly'); ?></ul>-->
-        <select name="archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'>
-        <option value=""><?php echo esc_attr(__('Select Month')); ?></option>
-        <?php wp_get_archives('type=monthly&format=option'); ?> </select> <br>
-        <?php endif; ?>
-        
-        <?php if (is_page_template('spanish.php')) : ?>
-        	<a href="/posts/spanish">Noticias en Espa&ntilde;ol &gt;</a>
-		<?php elseif (is_page('action')) : ?>
-        	<a href="/posts/actionalert">All Action Alerts &gt;</a>
-        <?php elseif (is_page('resources')) : ?>
-			<a href="/posts/resources">More Resources &gt;</a>
-        <?php else: ?>
-        	<!--<a href="/news">More News &gt;</a>-->
-		<?php endif; ?>
-        
-        
-        
-        <!-- Video/audio sidebar -->
-        <?php 
-		$news_cat = 884; 
-		$news_title="Latest Video &amp; Audio";
-		$posts_num = 2;
-		$showing_videos=false;
 		?>
-        <h4><?php echo $news_title; ?></h4>
-        <ul>
-        <?php
-		// Get homepage posts 
-		 if ($news_cat != '206') $lastposts = get_posts('numberposts='.$posts_num.'&category='.$news_cat.',-206');
-		 else $lastposts = get_posts('numberposts='.$posts_num.'&category='.$news_cat);
-		 foreach($lastposts as $post) :
-			setup_postdata($post);
-		 ?>
-         <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
-         <?php $showing_videos=true; ?>
-		 <?php if ($news_cat != '206') : ?><?php the_time('F jS, Y') ?>
-		 <?php else: ?><?php echo omi_spanish_date(the_date('j F Y','','',FALSE)); ?>
-		 <?php endif; ?></li>
-         <?php endforeach; ?>
-        </ul>
-        <?php if ($showing_videos) : ?><a href="/resources/video/">More video &amp; audio &gt;</a><?php endif; ?>
-        <!-- end video/audio sidebar -->
-        
+		<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
+		<?php if ($news_cat != '206') : ?><?php the_time('F jS, Y') ?>
+		<?php else: ?><?php echo omi_spanish_date(the_date('j F Y','','',FALSE)); ?>
+		<?php endif; ?></li>
+		<?php endforeach; ?>
+	</ul>
 
-    </div>
-<?php // endif ; ?>
+	<?php // Show more links only on the News pages ?>
+	<?php if(is_single() || !is_page()) : ?>
+	<h4>News Feed</h4>
+	<ul><li><a href="<?php bloginfo_rss('rss_url') ?>">Subscribe to RSS/XML feed</a></li></ul>
+	<h4>News Archives</h4>
+	<!--<ul><?php wp_get_archives('type=monthly'); ?></ul>-->
+	<select name="archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+	<option value=""><?php echo esc_attr(__('Select Month')); ?></option>
+	<?php wp_get_archives('type=monthly&format=option'); ?> </select> <br>
+	<?php endif; ?>
+	
+	<?php if (is_page_template('spanish.php')) : ?>
+		<a href="/posts/spanish">Noticias en Espa&ntilde;ol &gt;</a>
+	<?php elseif (is_page('action')) : ?>
+		<a href="/posts/actionalert">All Action Alerts &gt;</a>
+	<?php elseif (is_page('resources')) : ?>
+		<a href="/posts/resources">More Resources &gt;</a>
+	<?php else: ?>
+		<!--<a href="/news">More News &gt;</a>-->
+	<?php endif; ?>
+
+	<!-- Video/audio sidebar -->
+	<?php 
+	$news_cat = 884; 
+	$news_title="Latest Video &amp; Audio";
+	$posts_num = 2;
+	$showing_videos=false;
+	?>
+	<h4><?php echo $news_title; ?></h4>
+	<ul>
+	<?php
+	// Get homepage posts 
+	 if ($news_cat != '206') $lastposts = get_posts('numberposts='.$posts_num.'&category='.$news_cat.',-206');
+	 else $lastposts = get_posts('numberposts='.$posts_num.'&category='.$news_cat);
+	 foreach($lastposts as $post) :
+		setup_postdata($post);
+	 ?>
+	 <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
+	 <?php $showing_videos=true; ?>
+	 <?php if ($news_cat != '206') : ?><?php the_time('F jS, Y') ?>
+	 <?php else: ?><?php echo omi_spanish_date(the_date('j F Y','','',FALSE)); ?>
+	 <?php endif; ?></li>
+	 <?php endforeach; ?>
+	</ul>
+	<?php if ($showing_videos) : ?><a href="/resources/video/">More video &amp; audio &gt;</a><?php endif; ?>
+	<!-- end video/audio sidebar -->
+</div>
